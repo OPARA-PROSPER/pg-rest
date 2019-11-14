fea0ab98a3bf4894474d298a994c4c3cbf285604
Opara Prosper <oparaprosper79@gmail.com>
2019-11-14 19:02:31 +0100

    Add test for endpoints

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,18 +1,50 @@
-const request = require('request');
+const rp = require('request-promise');
 
 describe('test', function(){
+
+  const getEndPoint = {
+    method: 'GET',
+    uri: 'http://localhost:3000/students',
+    resolveWithFullResponse: true
+  };
+
+  const postEndPoint = {
+    method: 'POST',
+    uri: 'http://localhost:3000/students',
+    form: {
+      "name": "opara festus",
+      "age": 30,
+      "class": "500L",
+      "guardian_contact": "57 New Umuahia Road Aba",
+      "admission_date": "Feb 2011"
+    },
+    headers: {
+      'content-type': 'application/json'
+    },
+    resolveWithFullResponse: true
+  }
+
+  /*
+  / Status code for the API endpoints
+  */ 
+  let getStatusCode = 0;
+  let postStatusCode = 0;
+
+  beforeEach(async function(){
+    await rp(getEndPoint).then((res) => {
+      getStatusCode = res.statusCode;
+    })
+
+    await rp(postEndPoint).then((res) => {
+      postStatusCode = res.statusCode;
+    })
+  });
   
-  it('/', function(){
-    let status = 2;
-    const get_end_point = 'http://localhost:3000/students'
-
-    request(get_end_point, (error, response, body) =>{
-      expect(response.statusCode).toEqual(400);
-      console.log(status);
-    });
-
-    
+  it('get verb', function(){
+    expect(status).toBe(200);
   });
 
-  // it()
+  it('post verb', function(){
+    expect(postStatus).toBe(200);
+  })
 })
